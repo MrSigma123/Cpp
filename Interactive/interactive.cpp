649b5bf4ad9f5f01f3952a37aa4e543d1cea3c67
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-07 23:55:43 +0200

    Create miniature interactive program

---
+++ Interactive/interactive.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+  int value;
+  cout << "Enter your favorite number: ";
+  cin >> value;
+  cout << "Your favorite number is: " << value << endl;
+  cout << endl << "It's really cool number!";
+}
