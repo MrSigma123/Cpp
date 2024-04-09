f06ec5f44ffcb069251803846d6a7eed994d9c54
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-10 00:38:42 +0200

    Fix ordering and add newline characters to completed ASCII arts

--- Switch/switch.cpp
+++ Switch/switch.cpp
@@ -6,28 +6,33 @@ int main(void){
   cout << "Choose the option (enter value from 0 to 10): ";
   cin >> choice;
   switch(choice){
-    case 1:
+    case 0:
       cout << "You have chosen:" << endl;
       cout << " 0000\n";
       cout << "00  00\n";
       cout << "00  00\n";
       cout << "00  00\n";
       cout << " 0000\n";
-    case 2:
+      break;
+    case 1:
       cout << "You have chosen:" << endl;
       cout << "1111\n";
       cout << "  11\n";
       cout << "  11\n";
       cout << "  11\n";
       cout << "111111\n";
+      break;
+    case 2:
+      cout << "You have chosen:" << endl;
+      cout << " 2222\n";
+      cout << "22  22\n";
+      cout << "   22\n";
+      cout << "  22\n";
+      cout << "222222\n";
+      break;
     case 3:
       cout << "You have chosen:" << endl;
-      cout << " 2222";
-      cout << "22  22";
-      cout << "   22";
-      cout << "  22";
-      cout << "222222";
-
+       
     case 4:
       cout << "You have chosen:";
 
